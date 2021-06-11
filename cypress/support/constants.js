96b4f23c32169abd73059f019246e0f0481be237
shmck <shawn.j.mckay@gmail.com>
2021-06-10 19:38:56 -0700

    3.2S constants

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -1,2 +1,12 @@
 // Constants
 
+export const LOGIN_USERS = {
+    LOCKED: {
+        username: 'locked_out_user',
+        password: 'secret_sauce',
+    },
+    STANDARD: {
+        username: 'standard_user',
+        password: 'secret_sauce',
+    },
+};
