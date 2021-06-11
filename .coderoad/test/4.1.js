b4cf2c1032394b4fba3557b725bfc359b2c6b3ab
shmck <shawn.j.mckay@gmail.com>
2021-06-10 21:20:52 -0700

    4.1 login screen visible

---
+++ .coderoad/test/4.1.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const fsPromises = require('fs').promises;
+
+describe('login.spec.js', () => {
+    let file
+    before(async () => {
+        try {
+            const path = '../cypress/integration/login.spec.js'
+            file = await fsPromises.readFile(path, 'utf-8');
+            expect(file).to.be.ok;
+        } catch (error) {
+            throw ('Errors', error);
+        }
+    })
+    it('should have a test for the LoginPage screen visibility', () => {
+        const result = file.match(/it.+\n\s+LoginPage.screen.should\([\'\"]be.visible[\'\"]\)/)
+        expect(!!result).to.be.ok
+    })
+})
