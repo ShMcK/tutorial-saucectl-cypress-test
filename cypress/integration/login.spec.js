40257d85f4ce2525dd75e44983d6048fdfac4c5b
shmck <shawn.j.mckay@gmail.com>
2021-06-10 21:25:21 -0700

    4.2S login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -11,4 +11,9 @@ describe('LoginPage', () => {
     it('should be able to test loading of login page', () => {
         LoginPage.screen.should('be.visible');
     });
+
+    it('should be able to login with a standard user', () => {
+        LoginPage.signIn(LOGIN_USERS.STANDARD);
+        SwagOverviewPage.screen.should('be.visible');
+    });
 });
