856af0bcc4a2827015c808f7951bd771d137cc28
shmck <shawn.j.mckay@gmail.com>
2021-06-10 20:58:21 -0700

    3.3S LoginPage PO

--- cypress/pageobjects/LoginPage.js
+++ cypress/pageobjects/LoginPage.js
@@ -1,1 +1,25 @@
 // Page Objects
+class LoginPage {
+    get screen() {
+        return cy.get('#login_button_container');
+    }
+
+    get username() {
+        return cy.get('#user-name');
+    }
+
+    get password() {
+        return cy.get('#password');
+    }
+
+    get loginButton() {
+        return cy.get('.btn_action');
+    }
+
+    get errorMessage() {
+        return cy.get('[data-test="error"]');
+    }
+
+}
+
+export default new LoginPage();
